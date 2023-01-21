14bc50dc1c2524a11fe44afd1df5f8a7a012e8ae
johnny14 <johnnyferns@gmail.com>
2023-01-21 17:34:43 +0000

    Added functionality to change color scheme

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ cardContent.classList.add("no-display");
 imageContainer.classList.remove("aboutme-details");
 
 });
+
+        // Navigation bar
+
+let spanSwitch = document.getElementById('switchspan');
+let controller = document.getElementById('btnctrl');
+let lightWrapper = document.getElementById('light-wrapper');
+let navLinks = document.querySelectorAll(".nav-links a");
+console.log(navLinks);
+
+spanSwitch.addEventListener('click', function () {
+    lightWrapper.classList.toggle('lightwrappertoggle');
+    controller.classList.toggle('righttoggle');
+    document.body.classList.toggle('styletoggle');
+    spanSwitch.classList.toggle('switchtoggle');
+
+    navLinks.forEach(function(link) {
+        link.classList.toggle('linkcolor');
+    })
+
+});
