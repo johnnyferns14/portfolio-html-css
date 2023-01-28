89c80959336f99426e63ec027ee9c34a0d9cd5aa
johnny14 <johnnyferns@gmail.com>
2023-01-28 20:21:41 +0000

    Added contact section with styling and js

--- main.js
+++ main.js
@@ -32,3 +32,67 @@ spanSwitch.addEventListener('click', function () {
     })
 
 });
+
+const contactSection = document.getElementById("contactSection");
+const contactForm = document.getElementById('myForm');
+const queryName = document.querySelector('input[name="name"]');
+const queryEmail = document.querySelector('input[name="email"]');
+const queryContact = document.querySelector('input[name="contact"]');
+const queryComment = document.getElementById('queryArea');
+const btnSubmit = document.querySelector('input[type="submit"]');
+const pElement = document.querySelector('.pContact');
+const numnum = document.getElementById('num');
+const sidebar = document.querySelector(".sidebar");
+
+// Enable / Disable the form button
+function btnDisplay() {
+
+const textInputs = [queryName, queryEmail, queryContact, queryComment];
+for (let i=0; i<textInputs.length; i++) {
+  textInputs[i].addEventListener('keyup', function(e) {
+
+    if (queryName.value == "" || queryEmail.value == "" || queryContact.value == "" || queryComment.value == "") {
+      btnSubmit.disabled=true;
+    }
+    else {
+      btnSubmit.disabled=false;
+    }
+  });
+}
+
+}
+
+btnDisplay();
+
+
+//button click event
+btnSubmit.addEventListener('click', function(e) {
+    alert(`Thank you ${queryName.value} for entering the information 
+  but the form was not submitted since the functionality
+  has not been added as yet. Stay tuned!!`);
+  contactForm.reset();
+
+});
+
+
+//contact form toggle
+
+
+
+function contactFormToggle() {
+  sidebar.addEventListener('click', function(event) {
+   // if(contactForm != event.target) return;
+    if (contactSection.style.right === '-16.2em') {
+      contactSection.style.right = "-0.2em";
+      contactSection.style.transition = "right 0.7s ease-in";
+    }
+
+    else {
+      contactSection.style.right = "-16.2em";
+      contactSection.style.transition = "right 0.7s ease-in";
+    }
+
+  });
+};
+
+contactFormToggle();
